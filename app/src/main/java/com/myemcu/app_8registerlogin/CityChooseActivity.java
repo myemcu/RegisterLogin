9545b666de19c965ebf2fb085138b3c21600ce8c
myemcu <1142497680@qq.com>
2016-08-04 16:50:09 +0800

    V1，XML文件布局。

---
+++ app/src/main/java/com/myemcu/app_8registerlogin/CityChooseActivity.java
@@ -0,0 +1,12 @@
+package com.myemcu.app_8registerlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CityChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
